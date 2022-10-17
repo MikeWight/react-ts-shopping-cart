6d611f505c4821fe3438f71851e66d9e49672ad4
MikeWight <michel.wight@hotmail.com>
2022-10-16 22:19:40 -0300

    wds 2029

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,10 @@
+type StoreItemProps = {
+    id: number;
+    name: string;
+    price: number;
+    imgUrl: string;
+};
+
+export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+    return <h1>Store Item</h1>;
+}
