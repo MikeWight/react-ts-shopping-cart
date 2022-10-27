292f2f2ee9bf55435d3aa7670fb0f719e0b6320e
MikeWight <michel.wight@hotmail.com>
2022-10-26 23:26:10 -0300

    finished. wow custom hook

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -6,6 +6,7 @@ import {
 	useState,
 } from "react";
 import { ShoppingCart } from "./../components/ShoppingCart";
+import { useLocalStorage } from "./../hooks/useLocalStorage";
 
 type ShoppingCartProviderProps = {
 	children: ReactNode;
@@ -35,7 +36,10 @@ export function useShoppingCart() {
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
 	const [isOpen, setIsOpen] = useState<boolean>(false);
-	const [cartItems, setCartItems] = useState<CartItem[]>([]);
+	const [cartItems, setCartItems] = useLocalStorage<CartItem[]>(
+		"shopping-cart",
+		[]
+	);
 	const cartQuantity = cartItems.reduce(
 		(quantity, item) => item.quantity + quantity,
 		0
